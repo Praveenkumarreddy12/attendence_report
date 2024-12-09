9c5067f113a213aaca39d84af340c081a6d7744b
praveen <149228650+Praveenkumarreddy12@users.noreply.github.com>
2024-12-09 12:59:37 +0530

    added database

--- eats/empl_attendence/models.py
+++ eats/empl_attendence/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+    emo_name = models.CharField(max_length=100)
+    empid = models.CharField(max_length=100)
+    emp_email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    image = models.ImageField(upload_to='images/')
+
+
